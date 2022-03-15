656b29705f301ce2a61f92d16f613c8b57a14495
Jyotinag <jyotinag.csedu@gmail.com>
2022-03-15 23:18:29 +0600

    9x9 Matrix Implementation

--- sparse.cpp
+++ sparse.cpp
@@ -4,16 +4,22 @@ using namespace std;
 
 int main()
 {
-    int sparse_matrix[3][3] =
+    int sparse_matrix[9][9] =
     {
-        {1,0,0},
-        {0,4,0},
-        {0,0,7}
+    {1,0,0,2,0,0,3,0,0},
+    {0,4,0,0,9,0,0,1,0},
+    {0,0,7,0,0,8,0,0,3},
+    {1,0,0,2,0,0,3,0,0},
+    {0,4,0,0,9,0,0,1,0},
+    {0,0,7,0,0,8,0,0,3},
+    {1,0,0,2,0,0,3,0,0},
+    {0,4,0,0,9,0,0,1,0},
+    {0,0,7,0,0,8,0,0,3}
     };
 
     map<pair<int,int>,int> new_matrix;
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
             if(sparse_matrix[i][j]!=0){
                 new_matrix[make_pair(i,j)]=sparse_matrix[i][j];
             }
